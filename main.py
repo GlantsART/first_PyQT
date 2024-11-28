ac88f27a171ab861e66570ff026195417b18778a
Гланц Артём <glants2008@yandex.ru>
2024-11-28 17:01:06 +0700

    переделанно без ui файла

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from random import randint
 from PyQt6 import uic
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPointF
 
@@ -9,15 +9,22 @@ from PyQt6.QtCore import QPointF
 class First(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.initUI()
         self.flag = False
         self.pushButton.clicked.connect(self.run)
+
+    def initUI(self):
+        self.setGeometry(100, 100, 750, 600)
+        self.setWindowTitle('Рисование кругов')
+        self.pushButton = QPushButton('Это кнопка!', self)
+        self.pushButton.resize(120, 100)
+        self.pushButton.move(40, 50)
 
     def paintEvent(self, event):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             coords = randint(20, 250)
             qp.drawEllipse(QPointF(400, 300), coords, coords)
         self.flag = False
